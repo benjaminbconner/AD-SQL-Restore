6883e169759b28972fd380fb5184566d461d801a
benjaminbconner <82186116+benjaminbconner@users.noreply.github.com>
2021-04-08 20:03:19 -0500

    Add files to publish publicly

---
+++ Contacts.sql
@@ -0,0 +1,11 @@
+use ClientDB
+CREATE TABLE Client_A_Contacts
+(
+first_name varchar(30),
+last_name varchar(30),
+city varchar(30),
+county varchar(30),
+zip varchar(30),
+officePhone varchar(30),
+mobilePhone varchar(30)
+)
---
+++ restore.ps1
@@ -0,0 +1,94 @@
+﻿# AD-SQL Restore
+
+Clear-Host
+
+New-ADOrganizationalUnit -Name finance -ProtectedFromAccidentalDeletion $False
+
+$NewAD = Import-CSV $PSScriptRoot\financePersonnel.csv
+$Path = "OU=finance,DC=anycompany,DC=com"
+
+foreach ($ADUser in $NewAD)
+{
+
+$First = $ADUser.fname
+$Last = $ADUser.Lname
+$Name = $First + " " + $Last
+$City = $ADUser.City
+$County = $ADUser.County
+$PostalCode = $ADUser.PostalCode
+$Office = $ADUser.OPhone
+$Mobile = $ADUser.MobilePhone
+
+New-ADUser -GivenName $First -Surname $Last -Name $Name -DisplayName $Name -City $City -State $County -PostalCode $PostalCode -OfficePhone $Office -MobilePhone $MobilePhone -Path $Path
+}
+
+
+
+Import-Module -Name sqlps -DisableNameChecking -Force
+
+##Create object for SQL connection
+$srv = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList ucertify1
+$db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -ArgumentList $srv, ClientDB
+$db.Create()
+
+Invoke-Sqlcmd -ServerInstance anycompany1 -Database ClientDB -InputFile $PSScriptRoot\Contacts.sql
+
+$table = 'Client_A_Contacts'
+$db = 'ClientDB'
+
+Import-CSV $PSScriptRoot\NewClientData.csv | ForEach-Object {Invoke-Sqlcmd -Database ClientDB -ServerInstance anycompany1 -Query "insert into $table (first_name,last_name, city, county, zip, officePhone, mobilePhone) VALUES `
+('$($_.first_name)','$($_.last_name)','$($_.city)','$($_.county)','$($_.zip)','$($_.officePhone)','$($_.mobilePhone)')"
+}
+
+
+try {
+
+# AD-SQL Restore
+
+Clear-Host
+
+New-ADOrganizationalUnit -Name finance -ProtectedFromAccidentalDeletion $False
+
+$NewAD = Import-CSV $PSScriptRoot\financePersonnel.csv
+$Path = "OU=finance,DC=anycompany,DC=com"
+
+foreach ($ADUser in $NewAD)
+{
+
+$First = $ADUser.fname
+$Last = $ADUser.Lname
+$Name = $First + " " + $Last
+$City = $ADUser.City
+$County = $ADUser.County
+$PostalCode = $ADUser.PostalCode
+$Office = $ADUser.OPhone
+$Mobile = $ADUser.MobilePhone
+
+New-ADUser -GivenName $First -Surname $Last -Name $Name -DisplayName $Name -City $City -State $County -PostalCode $PostalCode -OfficePhone $Office -MobilePhone $MobilePhone -Path $Path
+}
+
+
+
+Import-Module -Name sqlps -DisableNameChecking -Force
+
+##Create object for SQL connection
+$srv = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList ucertify1
+$db = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -ArgumentList $srv, ClientDB
+$db.Create()
+
+Invoke-Sqlcmd -ServerInstance anycompany1 -Database ClientDB -InputFile $PSScriptRoot\Contacts.sql
+
+$table = 'Client_A_Contacts'
+$db = 'ClientDB'
+
+Import-CSV $PSScriptRoot\NewClientData.csv | ForEach-Object {Invoke-Sqlcmd -Database ClientDB -ServerInstance anycompany1 -Query "insert into $table (first_name,last_name, city, county, zip, officePhone, mobilePhone) VALUES `
+('$($_.first_name)','$($_.last_name)','$($_.city)','$($_.county)','$($_.zip)','$($_.officePhone)','$($_.mobilePhone)')"
+}
+
+}
+Catch [System.OutOfMemoryException] {
+    Write-Output "OutOfMemoryException occured."
+}
+Catch {
+    Write-Output "Something else happened: $($_.Exception.message)"
+}
